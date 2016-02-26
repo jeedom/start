3c7635e7ac668f94cabf6e669922c66fd8cd3a69
Loïc <loic@jeedom.com>
2016-02-26 20:05:25 +0100

    Typo

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
-header( 'content-type: text/html; charset=utf-8' );
+header('content-type: text/html; charset=utf-8');
 
 $locale = "fr_FR.UTF-8";
-if (isSet($_GET["locale"])) $locale = $_GET["locale"];
+if (isSet($_GET["locale"])) {
+	$locale = $_GET["locale"];
+}
+
 putenv("LC_MESSAGES=$locale");
 setlocale(LC_MESSAGES, $locale);
 bindtextdomain("index", "./locale");
@@ -11,11 +14,11 @@ textdomain("index");
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Jeedom - <?= _("Guides de Démarrage");?></title>
+        <title>Jeedom - <?=_("Guides de Démarrage");?></title>
         <meta charset="utf-8">
         <link rel="icon" href="favicon.ico" type="image/x-icon"/>
-        <script src="3rdparty/jquery/jquery.min.js"></script> 
-        <script src="3rdparty/bootstrap/bootstrap.min.js"></script> 
+        <script src="3rdparty/jquery/jquery.min.js"></script>
+        <script src="3rdparty/bootstrap/bootstrap.min.js"></script>
         <script src="3rdparty/jquery.steps/jquery.steps.min.js"></script>
         <link href="css/flag-icon.min.css" rel="stylesheet">
         <link href="css/style.css" rel="stylesheet">
@@ -27,13 +30,13 @@ textdomain("index");
     	<div class="row">
     		<div class="col-md-4"></div>
   			<div class="col-md-1"><img alt="Jeedom" src="img/logo.png" style="width: 70px;"></div>
-  			<div class="col-md-4"><h2><?= _("Guides de Démarrage");?></h2></div>
+  			<div class="col-md-4"><h2><?=_("Guides de Démarrage");?></h2></div>
   			<div class="col-md-3"><h2>
   			<a href="index.php?locale=fr_FR.UTF-8" class="btn btn-sm btn-info"><span class="flag-icon flag-icon-fr"></span> Français</a> &nbsp; <a href="index.php?locale=en_US.UTF-8" class="btn btn-sm btn-info"><span class="flag-icon flag-icon-gb"></span> English</a>
   			</h2></div>
     	</div>
     	<div class="row" style="background: #96c927;">
-    	<h2><center><span style="color: white;"><?= _("Quel pack possédez-vous ?");?></span></center></h2>
+    	<h2><center><span style="color: white;"><?=_("Quel pack possédez-vous ?");?></span></center></h2>
     	</div>
     	<div class="row" style="background: #96c927;">
 	    	<br><br>
@@ -41,13 +44,13 @@ textdomain("index");
 	    	<div class="col-md-5">
     			<div class="thumbnail">
     				<img alt="mini+" src="img/pack-zwave.jpg" style="width: 200px;">
-    				<div class="caption"><center><b>Pack Jeedom Mini+ Z-Wave</b><br><a href="base.php?locale=<?php echo $locale; ?>&type=base" class="btn btn-primary"><?= _("Accéder au guide");?></a></center></div>
+    				<div class="caption"><center><b>Pack Jeedom Mini+ Z-Wave</b><br><a href="base.php?locale=<?php echo $locale; ?>&type=base" class="btn btn-primary"><?=_("Accéder au guide");?></a></center></div>
     			</div>
     		</div>
     		<div class="col-md-5">
     			<div class="thumbnail">
     				<img alt="sd-card" src="img/pack-zwave-rfxcom.jpg" style="width: 200px;">
-    				<div class="caption"><center><b>Pack Jeedom Mini+ Z-Wave + Rfxcom</b><br><a href="base.php?locale=<?php echo $locale; ?>&type=rfxcom" class="btn btn-primary"><?= _("Accéder au guide");?></a></center></div>
+    				<div class="caption"><center><b>Pack Jeedom Mini+ Z-Wave + Rfxcom</b><br><a href="base.php?locale=<?php echo $locale; ?>&type=rfxcom" class="btn btn-primary"><?=_("Accéder au guide");?></a></center></div>
     			</div>
     		</div>
     		<div class="col-md-1"></div>
@@ -56,15 +59,15 @@ textdomain("index");
     		<div class="col-md-5">
     			<div class="thumbnail">
     				<img alt="power-supply" src="img/pack-enocean.jpg"  style="width: 200px;">
-    				<div class="caption"><center><b>Pack Jeedom Mini+ Enocean</b><br><a href="base.php?locale=<?php echo $locale; ?>&type=enocean" class="btn btn-primary"><?= _("Accéder au guide");?></a></center></div>
+    				<div class="caption"><center><b>Pack Jeedom Mini+ Enocean</b><br><a href="base.php?locale=<?php echo $locale; ?>&type=enocean" class="btn btn-primary"><?=_("Accéder au guide");?></a></center></div>
     			</div>
-    		</div>	
+    		</div>
     		<div class="col-md-5">
     			<div class="thumbnail">
     				<img alt="power-supply" src="img/pack-enocean-rfxcom.jpg"  style="width: 200px;">
-    				<div class="caption"><center><b>Pack Jeedom Mini+ Enocean + Rfxcom</b><br><a href="base.php?locale=<?php echo $locale; ?>&type=enocean_rfxcom" class="btn btn-primary"><?= _("Accéder au guide");?></a></center></div>
+    				<div class="caption"><center><b>Pack Jeedom Mini+ Enocean + Rfxcom</b><br><a href="base.php?locale=<?php echo $locale; ?>&type=enocean_rfxcom" class="btn btn-primary"><?=_("Accéder au guide");?></a></center></div>
     			</div>
     		</div>
-    		<div class="col-md-1"></div>	
+    		<div class="col-md-1"></div>
 	    </div>
 	</body>
